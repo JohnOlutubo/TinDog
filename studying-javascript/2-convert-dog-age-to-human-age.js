6ed4869e36420c90ebc625cc472e55077c3ddede
JohnOlutubo <107564128+JohnOlutubo@users.noreply.github.com>
2022-11-03 16:06:30 +0100

    Convert dog age to human age

---
+++ studying-javascript/2-convert-dog-age-to-human-age.js
@@ -0,0 +1,11 @@
+// How old will your dog be if it was human?
+
+//create a prompt that asks the user the age of their dog.
+
+let dogAge = prompt("How old is your dog?");
+
+// Calculate the equivalent human age of dog using this formula 
+const humanAge = (dogAge - 2) * 4 + 21;
+
+// Give the answer back to the user through an alert message.
+alert("Your dog is " + humanAge + " years old in human years.");
